eb6d197514850a72bd4f3dea9373e244b0a2d44b
ChristianNemeno <christiannemeno80@gmail.com>
2026-04-18 18:34:19 +0800

    0(n), we do it the same calc in 1 pass

---
+++ python/0020_valid_parentheses/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        close = { 
+            ')' : '(',.
+            '}' : '{',
+            ']' : '[',
+         }
+        stack = []
+
+        for c in s:
+            if c in close:
+                #closed paren
+                if stack and stack[-1] == close[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                #means open just append 
+                stack.append(c)
+
+        return False if stack else True
+        
