f0ce3866ec837bafadd6d8ce69b04eb3ff78df94
ChristianNemeno <christiannemeno80@gmail.com>
2026-04-23 19:21:53 +0800

    sad

---
+++ python/dynamic_programming/0053_maxsubarray.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        curr = nums[0]
+        best = nums[0]
+
+        for i in range(1,len(nums)):
+            curr = max(nums[i], curr + nums[i])
+            best = max(curr, best)
+        
+        return best
+        
