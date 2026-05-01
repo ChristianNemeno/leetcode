c08d4b89bde79796072e61a1c164c9b34d775770
ChristianNemeno <christiannemeno80@gmail.com>
2026-05-01 19:15:17 +0800

    160

---
+++ python/linked_list/0160_intersection_two_ll.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        visited = {}
+
+        while headA or headB:
+
+
+            if headA == headB:
+                return headA
+            if headA in visited:
+                return headA
+            if headB in visited:
+                return headB
+
+            if headA:
+                visited[headA] = True
+                headA = headA.next
+            if headB:
+                visited[headB] = True
+                headB = headB.next
+            
+        return None
